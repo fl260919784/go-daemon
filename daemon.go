fa802906b995b10a0e90cd355c3d918206cb9e95
fl260919784 <fl260919784@gmail.com>
2020-06-17 15:59:43 +0800

    base version

---
+++ daemon.go
@@ -0,0 +1,28 @@
+package daemon
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+func Daemon() {
+	if syscall.Getppid() == 1 {
+		return
+	}
+
+	args := os.Args[1:]
+
+	cmd := exec.Command(os.Args[0], args...)
+	cmd.Stdin = nil
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setsid: true}
+
+	err := cmd.Start()
+	if err == nil {
+		cmd.Process.Release()
+		os.Exit(0)
+	}
+}
